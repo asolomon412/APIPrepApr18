22b4f81463bceb50a13dbf633b041ff05e67a843
Antonella Solomon <antonellasolomon@gmail.com>
2018-06-07 07:46:31 -0400

    made some updates

--- src/main/java/com/apiex/APIPrep/controller/HomeController.java
+++ src/main/java/com/apiex/APIPrep/controller/HomeController.java
@@ -16,56 +16,42 @@ import org.springframework.web.servlet.ModelAndView;
 import com.apiex.APIPrep.model.Person;
 import com.apiex.APIPrep.model.Quote;
 import com.apiex.APIPrep.model.Result;
-import com.apiex.APIPrep.model.weather.Weather;
 
 @Controller
 public class HomeController {
-	
+
 	@RequestMapping("/")
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("index");
 		RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        mv.addObject("test", quote);
+		Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+		mv.addObject("test", quote);
 		return mv;
 	}
-	
-	//https://forecast.weather.gov/MapClick.php?lat=42.331427&lon=-83.045754&FcstType=json
 
-	@RequestMapping("/weather")
-	public ModelAndView weather() {
-		ModelAndView mv = new ModelAndView("weather");
-		CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier())
-				.build();
-		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-		requestFactory.setHttpClient(httpClient);
-		RestTemplate restTemplate = new RestTemplate(requestFactory);
-        Weather quote = restTemplate.getForObject("https://forecast.weather.gov/MapClick.php?lat=42.331427&lon=-83.045754&FcstType=json", Weather.class);
-        System.out.println(quote);
-        mv.addObject("test", quote);
-		return mv;
-	}
-	
 	@RequestMapping("/people")
 	public String people() {
+		// these lines are needed for https
 		CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier())
 				.build();
 		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
 		requestFactory.setHttpClient(httpClient);
+		// need to pass in the request factory to the RestTemplate
 		RestTemplate restTemplate = new RestTemplate(requestFactory);
 		Result result = restTemplate.getForObject("https://swapi.co/api/people/?format=json", Result.class);
-		
+
 		// Don't need any of this, just used for testing
 		System.out.println(result.toString());
-		
+
 		ArrayList<Person> people = result.getResults();
-	
+
 		for (Person p : people) {
 			System.out.println(p.getName());
 		}
 		// end of testing
 		return "personselect";
 	}
+
 	@RequestMapping("/personspecs")
 	public String personSpecs(@RequestParam("person") int person, Model model) {
 		CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier())
@@ -76,8 +62,8 @@ public class HomeController {
 		Person result = restTemplate.getForObject("https://swapi.co/api/people/" + person + "/?format=json",
 				Person.class);
 		System.out.println(result.toString());
-		
-		model.addAttribute("person",result);
+
+		model.addAttribute("person", result);
 		return "personresults";
 	}
 
